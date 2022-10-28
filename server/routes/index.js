82b6a0bce8ab790ccca05a9e5e9340ec909de4ee
Eric Sayer <me@ericsayer.com>
2022-10-28 05:50:04 +0000

    removed extra gitignore

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,11 @@ const path = require('path');
 
 
 // serve up react front-end in production
+
+
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
+
 
 module.exports = router;
